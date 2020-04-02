635c95b00b125815413ae1fcf3d3e0ad75b4a39c
akinleyeoj <akinleyeoj@gmail.com>
2020-04-02 22:25:44 +0100

    add updates

--- my-app/src/pages/SingleRoom.js
+++ my-app/src/pages/SingleRoom.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class SingleRoom extends Component {
     render() {
-        return <div> hello from the room </div>;
+        return <div> hello from the single room </div>;
     }
 }
