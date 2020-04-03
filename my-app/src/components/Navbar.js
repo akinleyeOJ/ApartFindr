6b1d3c5985a4deed28161aa9f5a71a1d90ce0832
akinleyeoj <akinleyeoj@gmail.com>
2020-04-03 17:35:37 +0100

    add navbar setup completed

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -13,13 +13,25 @@ export default class Navbar extends Component {
     };
     render() {
         return (
-            <nav className="Navbar">
+            <nav className="navbar">
                 <div className="nav-center">
                    <div className="nav-header">
                        <Link to="/">
                            <img src={logo} alt="llsls" />
                        </Link>
+        <button type="button" className="nav-btn"
+        onClick={this.handleToggle}>
+          <FaAlignRight className="nav-icon" />
+        </button>
                    </div>
+                   <ul className={this.state.isOpen?"nav-links show-nav":"nav-links"}>
+                       <li>
+                           <Link to="/">Home</Link>
+                       </li>
+                       <li>
+                           <Link to="/rooms">Hostels</Link>
+                       </li>
+                   </ul>
                 </div>
             </nav>
         );
