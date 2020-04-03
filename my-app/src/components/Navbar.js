9b050e49aba1a4930ed4548eab49a0e8d5ce9c35
akinleyeoj <akinleyeoj@gmail.com>
2020-04-03 14:40:10 +0100

    add updates

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import logo from '../images/imggg.png';
+import {FaAlignRight} from 'react-icons/fa';
+import {Link} from 'react-router-dom'
+
 
 export default class Navbar extends Component {
+    state={
+        isOpen:false
+    };
+    handleToggle = () => {
+        this.setState({ isOpen:!this.state.isOpen });
+    };
     render() {
         return (
-            <div>
-                hello this is navbar
-            </div>
+            <nav className="Navbar">
+                <div className="nav-center">
+                   <div className="nav-header">
+                       <Link to="/">
+                           <img src={logo} alt="llsls" />
+                       </Link>
+                   </div>
+                </div>
+            </nav>
         );
     }
 }
