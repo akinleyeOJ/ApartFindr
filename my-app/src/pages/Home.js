e39970b65b0ee6802267c160a71e8be4038a6ae4
akinleyeoj <akinleyeoj@gmail.com>
2020-04-04 18:59:18 +0100

    add updates

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
 import Hero from "../components/Hero";
-
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
 
 export default function Home() {
-    return <Hero hero="defaultHero"  />;
+    return ( 
+        <Hero>
+            <Banner title="Luxurious Apartments" subtitle="deluxe rooms starting at #300,000">
+                <Link to="/rooms" className="btn-primary">Apartments
+                </Link>
+            </Banner>
+        </Hero>
+    );
+    
 }
