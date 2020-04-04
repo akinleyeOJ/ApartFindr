0858b79d271510ae3102511851f5006910dabd63
akinleyeoj <akinleyeoj@gmail.com>
2020-04-04 21:07:49 +0100

    add import react naira

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -2,15 +2,20 @@ import React from "react";
 import Hero from "../components/Hero";
 import Banner from '../components/Banner';
 import {Link} from 'react-router-dom';
+import Services from '../components/Services';
+import Naira from 'react-naira';
 
 export default function Home() {
     return ( 
+        <>
         <Hero>
             <Banner title="Luxurious Apartments" subtitle="deluxe rooms starting at #300,000">
                 <Link to="/rooms" className="btn-primary">Apartments
                 </Link>
             </Banner>
         </Hero>
+        <Services />
+        </>
     );
     
 }
