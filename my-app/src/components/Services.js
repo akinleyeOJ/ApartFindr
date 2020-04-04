e0c8e1733054148b05eca69f6b8d667b537763fe
akinleyeoj <akinleyeoj@gmail.com>
2020-04-04 18:56:53 +0100

    add services.js

---
+++ my-app/src/components/Services.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Tite from '/Title';
+
+export default class Services extends Component {
+    render() {
+        return (
+            <div>
+                hello from services
+            </div>
+        );
+    }
+}
