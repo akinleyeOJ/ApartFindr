9219969a2ee8f0a8ab3d865ba8966f05bfa2554f
akinleyeoj <akinleyeoj@gmail.com>
2020-04-06 21:16:37 +0100

    update services section

--- my-app/src/components/Services.js
+++ my-app/src/components/Services.js
@@ -3,11 +3,51 @@ import {FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
 import Title from './Title';
 
 export default class Services extends Component {
+    state={
+        services: [
+            {
+                icon:<FaCocktail />,
+                title:"free cocktails",
+                info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"
+
+            },
+            {
+                icon:<FaHiking />,
+                title:"free cocktails",
+                info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"
+
+            },
+            {
+                icon:<FaShuttleVan />,
+                title:"free cocktails",
+                info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"
+
+            },
+            {
+                icon:<FaBeer />,
+                title:"free cocktails",
+                info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"
+
+            }
+        ]
+    };
     render() {
         return (
-            <div>
+           <section className="services">
                 <Title title="services" />
-            </div>
+             <div className="services-center">
+                 {this.state.services.map((item,index) => {
+                     return ( 
+                     <article key={index} className="service">
+                         <span>{item.icon}</span>
+                         <h6>{item.title}</h6>
+                         <p>{item.info}</p>
+                     </article>
+                 );
+                 })}
+
+             </div>
+            </section>
         );
     }
 }
