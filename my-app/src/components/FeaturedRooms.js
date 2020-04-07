4ab2341c2a23bd3b027fa86611a07a34f4312acf
akinleyeoj <akinleyeoj@gmail.com>
2020-04-07 23:11:56 +0100

    add updatess

--- my-app/src/components/FeaturedRooms.js
+++ my-app/src/components/FeaturedRooms.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { RoomContext } from "../context";
+import Loading from "./loading";
 
 export default class FeaturedRooms extends Component {
+    static contextType = RoomContext;
     render() {
-        return (
-            <div>
-                
+        const { FeaturedRooms: rooms } = this.context;
+        console.log(rooms);
+
+        return   <div>
+                helloo <Loading />
             </div>
-        );
+        
     }
 }
 
