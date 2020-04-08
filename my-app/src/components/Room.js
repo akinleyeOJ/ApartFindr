5eff318c9a55add9be013c9c630745f742311137
akinleyeoj <akinleyeoj@gmail.com>
2020-04-09 00:02:29 +0100

    add room.js updates

--- my-app/src/components/Room.js
+++ my-app/src/components/Room.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+
 export default function Room ({room}) {
     const { name, slug, images, price } =room;
     return (
         <article className="room">
             <div className="img-container">
                 <img src={images[0]} alt="single room" />
+                <div className="price-top">
+                    <h6>${price}</h6>
+                    <p>per year</p>
+                </div>
         </div>
         </article>
     );
