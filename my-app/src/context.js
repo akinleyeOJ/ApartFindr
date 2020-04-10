0c6e22dfe2330a5b5a971c15b8d07633f9937676
akinleyeoj <akinleyeoj@gmail.com>
2020-04-11 00:56:20 +0100

    add updates

--- my-app/src/context.js
+++ my-app/src/context.js
@@ -9,20 +9,35 @@ class RoomProvider extends Component {
         rooms:[],
         sortedRooms:[],
         featuredRooms: [],
-        loading: true
+        loading: true,
+        type: 'all',
+        capacity: 1,
+        price: 0,
+        minPrice:0,
+        maxPrice: 0,
+        minSize: 0,
+        maxSize: 0,
+        breakfast: false,
+        pets: false
     };
-
     //getData
 
     componentDidMount(){
+        // this.getData
         let rooms = this.formatData(items);
         let featuredRooms = rooms.filter(room => room.featured === true);
+        let maxPrice = Math.max(...rooms.map(item => item.price));
+        let maxSize = Math.max(...rooms.map(item => item.size));
+        
         this.setState({
            rooms, 
            featuredRooms,
            sortedRooms:rooms,
-           loading: false
-        })
+           loading: false,
+           price: maxPrice,
+           maxPrice,
+           maxSize
+        });
     }
 
     formatData(items){
@@ -40,12 +55,23 @@ class RoomProvider extends Component {
         let tempRooms = [...this.state.rooms];
         const room = tempRooms.find(room => room.slug === slug);
         return room;
-    }
-
-
+    };
+    handleChange = event => {
+        const target = event.target
+        const value = event.type === 'checkbox' ?
+        target.checked:target.value
+        const name = event.target.name;
+        this.setState({
+            [name]:value
+        },this.filterRooms)
+    };
+    filterRooms = () => {
+        console.log("hello");
+    };
     render() {
         return ( 
-        <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
+        <RoomContext.Provider 
+          value={{ ...this.state, getRoom: this.getRoom }}>
             {this.props.children}
         </RoomContext.Provider>    
         );
